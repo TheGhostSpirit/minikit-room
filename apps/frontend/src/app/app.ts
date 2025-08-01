fae95e3c67b64fc6a3a79d2ce9bbadba2b8e0129
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-02 01:12:07 +0200

    feat: defined page structure

--- apps/frontend/src/app/app.ts
+++ apps/frontend/src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { Navbar } from './navbar/navbar';
+import { Navbar } from './components/navbar/navbar';
+import { Sidenav } from './components/sidenav/sidenav';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navbar],
+  imports: [RouterOutlet, Navbar, Sidenav],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
