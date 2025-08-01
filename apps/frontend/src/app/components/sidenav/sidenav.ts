7246956266842ab1034886e73969ab77d2c4d908
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-02 01:06:45 +0200

    feat: add sidenav

---
+++ apps/frontend/src/app/components/sidenav/sidenav.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+
+import { MenuItem } from 'primeng/api';
+import { MenuModule } from 'primeng/menu';
+
+@Component({
+  selector: 'app-sidenav',
+  imports: [MenuModule],
+  templateUrl: './sidenav.html',
+  styleUrl: './sidenav.scss'
+})
+export class Sidenav {
+
+  items: MenuItem[] = [
+    { label: 'Pixelthèque', routerLink: 'pixeltheque' },
+    { label: 'Bibliothèque' },
+    { label: 'Cinémathèque' },
+    { label: 'Ludothèque' },
+    { label: 'Armoire' },
+    { label: 'Livre de recettes' },
+  ];
+
+}
