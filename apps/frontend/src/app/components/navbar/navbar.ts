778d829dd43e186521337bb9981a1706d5f151c5
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-09 00:01:29 +0200

    chore: rework layout

--- apps/frontend/src/app/components/navbar/navbar.ts
+++ apps/frontend/src/app/components/navbar/navbar.ts
@@ -2,20 +2,17 @@ import { Component } from '@angular/core';
 
 import { MenuItem } from 'primeng/api';
 import { MenubarModule } from 'primeng/menubar';
-import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'app-navbar',
-  imports: [MenubarModule, AvatarModule],
+  imports: [MenubarModule],
   templateUrl: './navbar.html',
   styleUrl: './navbar.scss'
 })
 export class Navbar {
 
   items: MenuItem[] = [
-    { label: 'Home' },
-    { label: 'Features' },
-    { label: 'Projects' },
+    { label: 'Minikit-Room' }
   ];
 
 }
