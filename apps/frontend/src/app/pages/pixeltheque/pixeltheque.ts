50924b852263561b1259e48b6137347db45ba97c
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-10 18:49:24 +0200

    feat: pixeltheque moc

--- apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
+++ apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 
+import { TableModule } from 'primeng/table';
+import { ButtonModule } from 'primeng/button';
+
 @Component({
   selector: 'app-pixeltheque',
-  imports: [],
+  imports: [TableModule, ButtonModule],
   templateUrl: './pixeltheque.html',
   styleUrl: './pixeltheque.scss'
 })
 export class Pixeltheque {
-
+  games = [
+    { name: 'PowerWashSimulator', achievements: 13 },
+    { name: 'PowerWashSimulator', achievements: 13 },
+    { name: 'PowerWashSimulator', achievements: 13 },
+    { name: 'PowerWashSimulator', achievements: 13 },
+    { name: 'PowerWashSimulator', achievements: 13 }
+  ];
 }
