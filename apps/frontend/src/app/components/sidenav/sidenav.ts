23ac495e373ae8d47f3d02557126d762966a8939
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-10 21:59:56 +0200

    feat: revamped sidenav

--- apps/frontend/src/app/components/sidenav/sidenav.ts
+++ apps/frontend/src/app/components/sidenav/sidenav.ts
@@ -1,28 +1,46 @@
 import { Component } from '@angular/core';
 
 import { MenuItem } from 'primeng/api';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { MenuModule } from 'primeng/menu';
+
+import { faPuzzlePiece, faGamepad, faVideo, faBook, faNoteSticky, faSkullCrossbones } from '@fortawesome/free-solid-svg-icons';
+import { faSteam, faXbox } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-sidenav',
-  imports: [MenuModule],
+  imports: [MenuModule, FontAwesomeModule],
   templateUrl: './sidenav.html',
   styleUrl: './sidenav.scss'
 })
 export class Sidenav {
 
   items: MenuItem[] = [
-    { label: 'Pixelthèque', items: [
-        { label: 'Steam' },
-        { label: 'Xbox' },
-        { label: 'Autres', routerLink: 'pixeltheque' },
+    {
+      label: 'Pixelthèque', items: [
+        { label: 'Steam', fa: faSteam, disabled: true },
+        { label: 'Xbox', fa: faXbox, disabled: true },
+        { label: 'Autres', routerLink: 'pixeltheque', fa: faGamepad },
       ]
     },
-    { label: 'Bibliothèque' },
-    { label: 'Cinémathèque' },
-    { label: 'Ludothèque' },
-    { label: 'Armoire' },
-    { label: 'Livre de recettes' },
+    {
+      label: 'Bibliothèque', items: [
+        { label: 'Livres', fa: faBook, disabled: true },
+        { label: 'Recettes', fa: faNoteSticky, disabled: true },
+      ]
+    },
+    {
+      label: 'Cinémathèque', items: [
+        { label: 'Films', fa: faVideo, disabled: true },
+      ]
+    },
+    {
+      label: 'Ludothèque', items: [
+        { label: 'Puzzles', fa: faPuzzlePiece, disabled: true },
+        { label: 'Jeux objets cachés', fa: faSkullCrossbones, disabled: true },
+      ]
+    },
   ];
 
 }
