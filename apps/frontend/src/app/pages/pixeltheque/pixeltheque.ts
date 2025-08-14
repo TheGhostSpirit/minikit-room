9a27f022f040e4c7d3a6b52d246e3f5ef2963cd8
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-15 00:38:18 +0200

    fix: temp pixeltheque layout

--- apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
+++ apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
@@ -2,14 +2,23 @@ import { Component } from '@angular/core';
 
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
+import { ToolbarModule } from 'primeng/toolbar';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrash, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-pixeltheque',
-  imports: [TableModule, ButtonModule],
+  imports: [TableModule, ButtonModule, FontAwesomeModule, ToolbarModule],
   templateUrl: './pixeltheque.html',
   styleUrl: './pixeltheque.scss'
 })
 export class Pixeltheque {
+  icons = {
+    trash: faTrash,
+    plus: faPlus,
+  };
+
   games = [
     { name: 'PowerWashSimulator', achievements: 13 },
     { name: 'PowerWashSimulator', achievements: 13 },
