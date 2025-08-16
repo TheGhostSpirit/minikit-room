1754045eb41c1c97595d73b67e1b8610e85c863d
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-16 21:58:06 +0200

    feat: finalize pixeltheque view layout v0

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { Pixeltheque } from './pages/pixeltheque/pixeltheque';
+import { EditGame } from './pages/edit-game/edit-game';
 
 export const routes: Routes = [
-  { title: 'Pixelthèque', path: 'pixeltheque', component: Pixeltheque }
+  { title: 'Pixelthèque', path: 'pixeltheque', component: Pixeltheque },
+  { title: 'Modifier mon jeu', path: 'game/edit/:id', component: EditGame }
 ];
--- apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
+++ apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 import { TableModule } from 'primeng/table';
 import { ButtonModule } from 'primeng/button';
 import { ToolbarModule } from 'primeng/toolbar';
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
 
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrash, faPlus } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-pixeltheque',
-  imports: [TableModule, ButtonModule, FontAwesomeModule, ToolbarModule],
+  imports: [TableModule, ButtonModule, FontAwesomeModule, ToolbarModule, InputTextModule, FloatLabelModule, RouterModule],
   templateUrl: './pixeltheque.html',
   styleUrl: './pixeltheque.scss'
 })
@@ -20,10 +23,10 @@ export class Pixeltheque {
   };
 
   games = [
-    { name: 'PowerWashSimulator', achievements: 13 },
-    { name: 'PowerWashSimulator', achievements: 13 },
-    { name: 'PowerWashSimulator', achievements: 13 },
-    { name: 'PowerWashSimulator', achievements: 13 },
-    { name: 'PowerWashSimulator', achievements: 13 }
+    { name: 'PowerWashSimulator', achievements: 13, id: 1 },
+    { name: 'PowerWashSimulator', achievements: 13, id: 2 },
+    { name: 'PowerWashSimulator', achievements: 13, id: 3 },
+    { name: 'PowerWashSimulator', achievements: 13, id: 4 },
+    { name: 'PowerWashSimulator', achievements: 13, id: 5 }
   ];
 }
