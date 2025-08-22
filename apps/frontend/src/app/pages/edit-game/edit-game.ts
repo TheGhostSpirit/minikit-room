6a67c99dd4a8a0d873b6065d707c0d2d5baca071
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-23 01:17:26 +0200

    feat: edit game view scaffold

--- apps/frontend/src/app/pages/edit-game/edit-game.ts
+++ apps/frontend/src/app/pages/edit-game/edit-game.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { InputTextModule } from 'primeng/inputtext';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { RatingModule } from 'primeng/rating';
+import { StepperModule } from 'primeng/stepper';
+import { ButtonModule } from 'primeng/button';
+
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-edit-game',
-  imports: [],
+  imports: [InputTextModule, FloatLabelModule, FormsModule, RatingModule, StepperModule, ButtonModule, FontAwesomeModule],
   templateUrl: './edit-game.html',
   styleUrl: './edit-game.scss'
 })
 export class EditGame {
+  faArrowRight = faArrowRight;
+  title = 'Modifier un jeu';
 
+  name = '';
+  rating = 2;
 }
