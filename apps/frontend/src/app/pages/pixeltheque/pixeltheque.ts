ad796eaabf880d1edd262a858a45e9b26c9ccfc9
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-08-30 01:26:42 +0200

    feat: begin integrate pixeltheque service

--- apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
+++ apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { AsyncPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
 
 import * as f from '@fortawesome/free-solid-svg-icons';
 
 import { CommonModule } from 'app/common-module';
+import { GameService } from 'app/services/game-service';
 
 @Component({
   selector: 'app-pixeltheque',
-  imports: [CommonModule],
+  imports: [CommonModule, AsyncPipe],
   templateUrl: './pixeltheque.html',
   styleUrl: './pixeltheque.scss'
 })
@@ -16,11 +18,7 @@ export class Pixeltheque {
     plus: f.faPlus,
   };
 
-  games = [
-    { name: 'PowerWashSimulator', achievements: 13, id: 1 },
-    { name: 'PowerWashSimulator', achievements: 13, id: 2 },
-    { name: 'PowerWashSimulator', achievements: 13, id: 3 },
-    { name: 'PowerWashSimulator', achievements: 13, id: 4 },
-    { name: 'PowerWashSimulator', achievements: 13, id: 5 }
-  ];
+  private readonly gameService = inject(GameService);
+
+  games = this.gameService.list();
 }
