2db733aa77a4ee5179ae40836ca5d7b1c133f8ea
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-09-02 01:29:10 +0200

    feat: game deletion

--- apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
+++ apps/frontend/src/app/pages/pixeltheque/pixeltheque.ts
@@ -1,9 +1,11 @@
 import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 
+import { Observable, Subject } from 'rxjs';
 import * as f from '@fortawesome/free-solid-svg-icons';
 
 import { CommonModule } from 'app/common-module';
+import { Game } from 'app/models/game';
 import { GameService } from 'app/services/game-service';
 
 @Component({
@@ -20,5 +22,9 @@ export class Pixeltheque {
 
   private readonly gameService = inject(GameService);
 
-  games = this.gameService.list();
+  games: Observable<Game[]> = this.gameService.list();
+
+  delete(id: number) {
+    this.gameService.delete(id).subscribe();
+  }
 }
