4a5d2445cfe54eb036af8da65f60e72fdbfeea88
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-09-30 16:48:42 +0200

    feat: homepage

--- apps/frontend/src/app/app.routes.ts
+++ apps/frontend/src/app/app.routes.ts
@@ -3,9 +3,12 @@ import { Routes } from '@angular/router';
 import { Pixeltheque } from 'app/pages/pixeltheque/pixeltheque';
 import { EditGame } from 'app/pages/edit-game/edit-game';
 import { CreateGame } from 'app/pages/create-game/create-game';
+import { Home } from 'app/pages/home/home';
 
 export const routes: Routes = [
+  { title: 'Index', path: '', component: Home },
   { title: 'Pixelthèque', path: 'pixeltheque', component: Pixeltheque },
   { title: 'Modifier un jeu', path: 'game/edit/:id', component: EditGame },
   { title: 'Créer un jeu', path: 'game/create', component: CreateGame },
+  { path: '**', redirectTo: '' },
 ];
