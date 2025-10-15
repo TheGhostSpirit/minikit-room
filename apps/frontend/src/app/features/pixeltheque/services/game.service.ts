8f5aae788dadc083e4c8451b8a223a280e7e3f3a
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-10-15 15:13:49 +0200

    fix: reactive errors in game service

--- apps/frontend/src/app/features/pixeltheque/services/game.service.ts
+++ apps/frontend/src/app/features/pixeltheque/services/game.service.ts
@@ -1,7 +1,7 @@
 import { inject, Injectable } from '@angular/core';
 
-import { BehaviorSubject, Observable, from } from 'rxjs';
-import { switchMap, tap } from 'rxjs/operators';
+import { BehaviorSubject, Observable, defer } from 'rxjs';
+import { first, switchMap, tap } from 'rxjs/operators';
 
 import { Game } from 'app/features/pixeltheque/models/game';
 import { IndexedDbService } from 'app/core/services/indexed-db/indexed-db.service';
@@ -18,30 +18,31 @@ export class GameService {
   private gamesData = this.gamesSubject.asObservable();
 
   list(): Observable<Game[]> {
-    return from(this.indexedDB.select<Game>(DB_KEYS.game)).pipe(
+    return defer(() => this.indexedDB.select<Game>(DB_KEYS.game)).pipe(
       tap((games) => this.gamesSubject.next(games)),
-      switchMap(() => this.gamesData)
+      switchMap(() => this.gamesData),
+      first(),
     );
   }
 
   findOne(id: number): Observable<Game> {
-    return from(this.indexedDB.selectOne<Game>(DB_KEYS.game, id));
+    return defer(() => this.indexedDB.selectOne<Game>(DB_KEYS.game, id)).pipe(first());
   }
 
   create(game: Game): Observable<Game[]> {
-    return from(this.indexedDB.add<Game>(DB_KEYS.game, game)).pipe(
+    return defer(() => this.indexedDB.add<Game>(DB_KEYS.game, game)).pipe(
       switchMap(() => this.list())
     );
   }
 
   delete(id: number): Observable<Game[]> {
-    return from(this.indexedDB.delete(DB_KEYS.game, id)).pipe(
+    return defer(() => this.indexedDB.delete(DB_KEYS.game, id)).pipe(
       switchMap(() => this.list())
     );
   }
 
   modify(id: number, game: Game): Observable<Game[]> {
-    return from(this.indexedDB.update<Game>(DB_KEYS.game, id, game)).pipe(
+    return defer(() => this.indexedDB.update<Game>(DB_KEYS.game, id, game)).pipe(
       switchMap(() => this.list())
     );
   }
