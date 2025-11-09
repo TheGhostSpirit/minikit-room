0e8bbf8d56a8e6fa94758369a79b9ce674a82dc9
TheGhostSpirit <quentin.caritey@outlook.fr>
2025-11-09 14:55:12 +0100

    feat: add basic search

--- apps/frontend/src/app/features/pixeltheque/pages/pixeltheque/pixeltheque.component.ts
+++ apps/frontend/src/app/features/pixeltheque/pages/pixeltheque/pixeltheque.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject } from '@angular/core';
 
-import { Observable } from 'rxjs';
+import { BehaviorSubject, combineLatest, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import * as f from '@fortawesome/free-solid-svg-icons';
 
 import { sharedDeclarations, sharedImports } from 'app/shared/shared.config';
@@ -22,6 +23,18 @@ export class PixelthequeComponent {
   private readonly gameService = inject(GameService);
 
   games$: Observable<Game[]> = this.gameService.list();
+  search$ = new BehaviorSubject<string>('');
+
+  filteredGames$ = combineLatest([this.games$, this.search$]).pipe(
+    map(
+      ([games, searchTerm]) =>
+        games.filter(game => game.name.toLowerCase().includes(searchTerm.toLowerCase()))
+    )
+  );
+
+  onSearch(term: string) {
+    this.search$.next(term);
+  }
 
   delete(id: number) {
     this.gameService.delete(id).subscribe();
