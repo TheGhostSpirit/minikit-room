b63d51f9ed8d471617fd8c8e212547318060217d
TheGhostSpirit <quentin.caritey@outlook.fr>
2026-02-18 18:16:33 +0100

    fix: fandom aliases

--- scripts/labyrinthine-cosmetic-scraper/src/aliases/fandom.ts
+++ scripts/labyrinthine-cosmetic-scraper/src/aliases/fandom.ts
@@ -2,7 +2,100 @@ import { CosmeticGroup } from 'models/cosmetic-groups';
 import { CosmeticType } from 'models/cosmetic-types';
 
 const groupAliases = new Map<CosmeticGroup, string[]>([
-  ['Hardcore', ['Hardcore cases']],
+  [
+    'Hardcore',
+    [
+      'Hardcore',
+      'Hardcore Cases',
+    ],
+  ],
+  [
+    'All Maps',
+    [
+      'All Mazes',
+    ],
+  ],
+  [
+    'Map/Monster Exclusive',
+    [
+      'Kept Hedges',
+      'Crypts',
+      'Forest',
+      'Unkept Hedges',
+      'Cornfield',
+      'Snowy Hedges',
+      'Sewer',
+      'Manor',
+      'Bamboo Forest',
+      'Trenches',
+      'Castle',
+      'Swamp',
+      'Carnival',
+      'Dead Forest',
+      'Mines',
+      'Backrooms',
+      'Ruins',
+      'Fog City',
+      'Pigman',
+      'Smiley',
+      'Wickerman',
+    ],
+  ],
+  [
+    'Summer',
+    [
+      'Summer',
+    ],
+  ],
+  [
+    'Easter',
+    [
+      'Easter',
+    ],
+  ],
+  [
+    'Halloween',
+    [
+      'Halloween',
+    ],
+  ],
+  [
+    'Christmas',
+    [
+      'Christmas',
+    ],
+  ],
+  [
+    'Valentine',
+    [
+      'Valentine',
+    ],
+  ],
+  [
+    `St Patrick's`,
+    [
+      'St. Patrick',
+    ],
+  ],
+  [
+    'Supporter Edition',
+    [
+      'Supporter DLC',
+    ],
+  ],
+  [
+    'Special',
+    [
+      'Shop',
+      'Legacy',
+      'Hidden',
+      'Reach level',
+      'Story Mode',
+      'Level 400',
+      'Private-Testing',
+      'Playing in VR',
+    ],
+  ],
 ]);
 
 const typeAliases = new Map<CosmeticType, string[]>([]);
