a8a510a51d538513ae233ed521834adb3e14ee06
TheGhostSpirit <quentin.caritey@outlook.fr>
2026-02-20 00:17:29 +0100

    feat: add concurrency limit for download

--- scripts/labyrinthine-cosmetic-scraper/src/steps/download-images.ts
+++ scripts/labyrinthine-cosmetic-scraper/src/steps/download-images.ts
@@ -1,23 +1,26 @@
 import fetch from 'node-fetch';
 import { writeFile, copyFile } from 'node:fs/promises';
+import pLimit from 'p-limit';
 
 import { Cosmetic } from '@mkr/shared/labyrinthine';
 
 import { getAssetsPath, getExportPath } from 'utils';
 
 export const downloadCosmeticImages = async (cosmetics: Cosmetic[]): Promise<unknown> => {
-  
+
+  const limit = pLimit(5);
+  const downloadCosmeticImage = (cosmetic: Cosmetic) => fetch(cosmetic.icon)
+    .then(data => data.arrayBuffer())
+    .then(data => writeFile(getExportPath(`${cosmetic.id}.img`), Buffer.from(data)));
+
   return Promise.all([
     ...cosmetics
       .filter(cosmetic => cosmetic.type !== 'Records')
       .map(cosmetic =>
-        fetch(cosmetic.icon)
-          .then(data => data.arrayBuffer())
-          .then(data => writeFile(getExportPath(`${cosmetic.id}.img`), Buffer.from(data)))
+        limit(() => downloadCosmeticImage(cosmetic))
       ),
     ...cosmetics
       .filter(cosmetic => cosmetic.type === 'Records')
       .map(cosmetic => copyFile(getAssetsPath('disc.png'), getExportPath(`${cosmetic.id}.img`)))
   ]);
 };
- 
