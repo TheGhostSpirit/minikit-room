3113ce00f11853f70db6973be734261574bd7f5c
TheGhostSpirit <quentin.caritey@outlook.fr>
2026-02-24 18:29:48 +0100

    chore: add mock commit data

--- apps/frontend/src/app/features/others/labyrinthine/components/commit-history/commit-history.component.ts
+++ apps/frontend/src/app/features/others/labyrinthine/components/commit-history/commit-history.component.ts
@@ -7,4 +7,47 @@ import { sharedImports } from 'app/shared/shared.config';
   imports: [...sharedImports],
   templateUrl: './commit-history.component.html',
 })
-export class CommitHistoryComponent {}
+export class CommitHistoryComponent {
+  commits = [
+    {
+      date: '2026-02-20T00:39:00.610Z',
+      cosmetics: [
+        { name: 'Golden Rose', type: 'Face' },
+        { name: 'White Rose', type: 'Face' },
+        { name: 'Red Rose', type: 'Face' },
+        { name: 'Black Rose', type: 'Face' },
+        { name: 'Blue Rose', type: 'Face' },
+        { name: 'Orange Rose', type: 'Face' },
+        { name: 'Pink Rose', type: 'Face' }
+      ],
+      id: 1
+    },
+    {
+      date: '2026-02-20T00:28:23.574Z',
+      cosmetics: [
+        { name: 'Black Watch', type: 'Wrist' },
+        { name: 'White Watch', type: 'Wrist' },
+      ],
+      id: 2
+    },
+    {
+      date: '2026-02-20T00:23:09.994Z',
+      cosmetics: [
+        { name: 'Green Flashlight Bulky', type: 'Flashlight' },
+        { name: 'Yellow Flashlight Bulky', type: 'Flashlight' },
+        { name: 'Blue Flashlight Bulky', type: 'Flashlight' },
+        { name: 'Red Flashlight Bulky', type: 'Flashlight' },
+      ],
+      id: 3
+    },
+    {
+      date: '2026-02-20T00:45:09.427Z',
+      cosmetics: [
+        { name: 'Pentagram Glowstick', type: 'Glowsticks' },
+        { name: 'Bone Glowstick', type: 'Glowsticks' },
+        { name: 'Skull Glowstick', type: 'Glowsticks' },
+      ],
+      id: 4
+    },
+  ];
+}
