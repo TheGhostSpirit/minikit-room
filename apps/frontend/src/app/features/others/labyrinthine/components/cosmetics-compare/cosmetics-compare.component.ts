5d80529f13033a3c860dcd205b498485557e0c85
TheGhostSpirit <quentin.caritey@outlook.fr>
2026-02-27 16:58:37 +0100

    feat: full compare page layout

--- apps/frontend/src/app/features/others/labyrinthine/components/cosmetics-compare/cosmetics-compare.component.ts
+++ apps/frontend/src/app/features/others/labyrinthine/components/cosmetics-compare/cosmetics-compare.component.ts
@@ -1,6 +1,9 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, inject, input } from '@angular/core';
+
+import * as f from '@fortawesome/free-solid-svg-icons';
 
 import { sharedImports } from 'app/shared/shared.config';
+import { ObjectCompressionService } from 'app/shared/services/object-compression.service';
 import { Cosmetic } from 'app/features/others/labyrinthine/models/cosmetic';
 
 @Component({
@@ -9,6 +12,27 @@ import { Cosmetic } from 'app/features/others/labyrinthine/models/cosmetic';
   templateUrl: './cosmetics-compare.component.html',
 })
 export class CosmeticsCompareComponent {
+  private readonly objectCompressionService = inject(ObjectCompressionService);
+
+  readonly icons = {
+    copy: f.faCopy,
+  };
+
   readonly cosmetics = input<Cosmetic[]>([]);
 
+  readonly foundCosmeticsHash = computed(() => {
+    const cosmetics = this.cosmetics();
+    const foundCosmetics  = cosmetics.filter(cosmetic => cosmetic.found).map(cosmetic => ({ name: cosmetic.name, type: cosmetic.type }));
+    return this.objectCompressionService.compress(foundCosmetics);
+  });
+
+  readonly customCosmetics1 = computed(() => {
+    const cosmetics = this.cosmetics();
+    return cosmetics.slice(0, 3);
+  });
+
+  readonly customCosmetics2 = computed(() => {
+    const cosmetics = this.cosmetics();
+    return cosmetics.slice(4, 6);
+  });
 }
