61adb6c73d9e607131cfcb470732e33be968ef3a
TheGhostSpirit <quentin.caritey@outlook.fr>
2026-03-02 18:15:12 +0100

    feat: multiple file upload

--- apps/frontend/src/app/shared/components/image-upload/image-upload.component.ts
+++ apps/frontend/src/app/shared/components/image-upload/image-upload.component.ts
@@ -14,10 +14,18 @@ export class ImageUploadComponent {
 
   readonly label = input<string>('Changer Image');
   readonly blobUrlScope = input<BlobUrlScope>();
+  readonly multiple = input<boolean>(false);
   readonly imageUploaded = output<[File, string]>();
+  readonly imagesUploaded = output<[File, string][]>();
+
+  upload(event: FileSelectEvent) {
+    const multiple = this.multiple();
+    multiple ? this.uploadImages(event) : this.uploadImage(event);
+  }
 
   uploadImage(event: FileSelectEvent) {
     const scope = this.blobUrlScope();
+
     const image = event.currentFiles[0];
 
     if (!scope) {
@@ -25,6 +33,20 @@ export class ImageUploadComponent {
     }
 
     this.imageUploaded.emit([image, scope.create(image)]);
+  }
+
+  uploadImages(event: FileSelectEvent) {
+    const scope = this.blobUrlScope();
+
+    const images = event.currentFiles;
+
+    if (!scope) {
+      throw new Error('No blob url scope specified');
+    }
+
+    this.imagesUploaded.emit(
+      images.map(image => ([image, scope.create(image)]))
+    );
   }
 
 }
