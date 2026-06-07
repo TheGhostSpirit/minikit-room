1f7ac363d8ffea4f48383f4f5325fab3b127711c
TheGhostSpirit <quentin.caritey@outlook.fr>
2026-06-07 17:19:13 +0200

    feat(gallery): made dynamic title and thumbnail

--- apps/frontend/src/app/features/mediatheque/pages/gallery/gallery.component.ts
+++ apps/frontend/src/app/features/mediatheque/pages/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, DestroyRef, inject } from '@angular/core';
 import { toSignal } from '@angular/core/rxjs-interop';
 
 import * as f from '@fortawesome/free-solid-svg-icons';
@@ -7,6 +7,7 @@ import { DialogService } from 'primeng/dynamicdialog';
 
 import { sharedDeclarations, sharedImports, sharedProviders } from 'app/shared/shared.config';
 import { AlbumService } from 'app/features/mediatheque/services/album.service';
+import { BlobUrlService } from 'app/shared/services/blob-url.service';
 import { Album } from 'app/features/mediatheque/models/album';
 import { CreateAlbumModalComponent } from 'app/features/mediatheque/components/create-album-modal/create-album-modal.component';
 
@@ -22,9 +23,20 @@ export class GalleryComponent {
   };
 
   private readonly albumService = inject(AlbumService);
+  private readonly blobUrlService = inject(BlobUrlService);
+  private readonly destroyRef = inject(DestroyRef);
   private readonly dialog = inject(DialogService);
+  private readonly blobUrlScope = this.blobUrlService.createScope(this.destroyRef);
 
   readonly albums = toSignal(this.albumService.list(), { initialValue: [] as Album[] });
+
+  readonly albumThumbnails = computed(() =>
+    new Map(
+      this.albums()
+        .filter(a => !!a.id && a.images.length > 0)
+        .map(a => [a.id!, this.blobUrlScope.create(a.images[0].data)])
+    )
+  );
 
   createAlbum() {
     this.dialog.open(
