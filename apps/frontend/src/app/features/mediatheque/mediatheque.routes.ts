756fd8f9ee989967a04cbbb1b2c499a5f80a5123
TheGhostSpirit <quentin.caritey@outlook.fr>
2026-09-08 16:08:25 +0200

    feat(album): edit album v0

--- apps/frontend/src/app/features/mediatheque/mediatheque.routes.ts
+++ apps/frontend/src/app/features/mediatheque/mediatheque.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { GalleryComponent } from 'app/features/mediatheque/pages/gallery/gallery.component';
 import { AlbumComponent } from 'app/features/mediatheque/pages/album/album.component';
+import { EditAlbumComponent } from 'app/features/mediatheque/pages/edit-album/edit-album.component';
 
 export const routes: Routes = [
   { title: 'Galerie', path: 'gallery', component: GalleryComponent },
   { title: 'Album', path: 'album/:id', component: AlbumComponent },
+  { title: 'Editer un album', path: 'album/:id/edit', component: EditAlbumComponent },
 ];
